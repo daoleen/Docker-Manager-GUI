0895c6c61bc2360fc1121ca76df1191995a96060
Alexander Kozlov <alexander.kozlov@hf-solution.com>
2019-11-10 17:32:13 +0300

    GEN-39 Recent projects * Save recent projects and load from recent

--- src/main/java/me/sunny/generator/docker/controller/ProjectOpenController.java
+++ src/main/java/me/sunny/generator/docker/controller/ProjectOpenController.java
@@ -3,12 +3,17 @@ package me.sunny.generator.docker.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
@@ -17,14 +22,47 @@ import javafx.util.Pair;
 import lombok.extern.slf4j.Slf4j;
 import me.sunny.generator.docker.Context;
 import me.sunny.generator.docker.exception.ApplicationException;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 
 @Slf4j
 public class ProjectOpenController {
+    private final String recentFileLocation = System.getProperty("user.home") + File.separator + ".docker-compose-generator" + File.separator + "recent.txt";
+    private final File recentFile = new File(recentFileLocation);
+
 
     @FXML
-    private ListView listRecentProjects;
+    private ListView<String> listRecentProjects;
+
+
+    @FXML
+    public void initialize() {
+        initRecentList();
+    }
+
+
+    private void initRecentList() {
+        try {
+            List<String> recentProjects = FileUtils.readLines(recentFile, "UTF-8");
+            listRecentProjects.setItems(FXCollections.observableList(recentProjects));
+        } catch (IOException e) {
+            log.debug("No recent projects found");
+        }
+    }
+
+
+    // open selected recent project on double-click
+    public void onMouseClickedOnRecentFile(MouseEvent mouseEvent) {
+        if (MouseButton.PRIMARY.equals(mouseEvent.getButton()) && 2 == mouseEvent.getClickCount()) {
+            String fileLocationStr = listRecentProjects.getSelectionModel().getSelectedItem();
+
+            if (StringUtils.isNotBlank(fileLocationStr)) {
+                File projectFile = new File(fileLocationStr);
+                openProject(projectFile);
+            }
+        }
+    }
 
 
     public void createNewProject(MouseEvent mouseEvent) {
@@ -54,14 +92,50 @@ public class ProjectOpenController {
         File projectFile = fileChooser.showOpenDialog(listRecentProjects.getScene().getWindow());
 
         if (projectFile != null) {
-            try {
-                Context.deserializeProject(projectFile);
-            } catch (ApplicationException e) {
-                Context.showNotificationDialog("Error opening project", e.getMessage(), Alert.AlertType.ERROR);
-                return;
-            }
+            openProject(projectFile);
+        }
+    }
 
-            openMainWindow();
+
+    private void openProject(File projectFile) {
+        try {
+            Context.deserializeProject(projectFile);
+        } catch (ApplicationException e) {
+            Context.showNotificationDialog("Error opening project", e.getMessage(), Alert.AlertType.ERROR);
+            return;
+        }
+
+        saveProjectToRecent(projectFile.getAbsolutePath());
+        openMainWindow();
+    }
+
+
+    private void saveProjectToRecent(String absolutePath) {
+        String recent = readRecent().replaceAll(absolutePath.concat("\n"), "");
+        recent = absolutePath.concat("\n").concat(recent);
+        saveRecent(recent);
+    }
+
+
+    private String readRecent() {
+        String recent;
+
+        try {
+            recent = FileUtils.readFileToString(recentFile, "UTF-8");
+        } catch (IOException e) {
+            log.debug("Creating a new recent file");
+            recent = "";
+        }
+        return recent;
+    }
+
+
+    private void saveRecent(String content) {
+        try {
+            FileUtils.writeStringToFile(recentFile, content, "UTF-8");
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+            Context.showNotificationDialog("Error", e.getMessage(), Alert.AlertType.WARNING);
         }
     }
 
@@ -106,5 +180,12 @@ public class ProjectOpenController {
         dialogStage.showAndWait();
 
         return new Pair<>(!dialogController.isCancelled(), dialogController.getContent());
+    }
+
+
+    public void removeRecentProject(ActionEvent actionEvent) {
+        String recentToRemove = listRecentProjects.getSelectionModel().getSelectedItem();
+        saveRecent(readRecent().replaceAll(recentToRemove.concat("\n"), ""));
+        initRecentList();
     }
 }
